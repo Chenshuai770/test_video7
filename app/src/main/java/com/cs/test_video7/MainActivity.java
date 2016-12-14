be914803fd0cc4ee33f35dce0f1f72691adec970
chenshuai <770456288@qq.com>
2016-12-14 17:44:22 +0800

    Initial commit

---
+++ app/src/main/java/com/cs/test_video7/MainActivity.java
@@ -0,0 +1,34 @@
+package com.cs.test_video7;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mBtn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mBtn = (Button) findViewById(R.id.btn);
+
+        mBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn:
+                startActivity(new Intent(this,PlayActivity.class));
+                break;
+        }
+    }
+}
